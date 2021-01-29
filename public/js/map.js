0f01db58196eb33cd06f233bab2e7987208f10aa
axyorah <elvira.bozileva@gmail.com>
2021-01-29 21:30:14 +0100

    add: added map popups with quick city info that appear when hovering over cities

--- public/js/map.js
+++ public/js/map.js
@@ -25,26 +25,27 @@ const map = new mapboxgl.Map({
     zoom: 6
 });
 
-// let cityMarker, popup;
-// for (let city of cities) {
-//     popup = new mapboxgl.Popup();
-//     popup.setHTML(`${city.name}<br>${city.quickInfo}`);
-//     cityMarker = new mapboxgl.Marker()
-//         .setLngLat(city.geometry.coordinates)
-//         .setPopup(popup)
-//         .addTo(map);
-// }
-
 map.on('load', function () {
     for (let area of areas) {
-        // add city-data of the current area
+        
+        // add city-data of the current area in correct format
         map.addSource(`cities-${area.name}`, {
             'type': 'geojson',
             'data': {
                 'type': 'FeatureCollection',
-                'features': area.cities
+                'features': area.cities.map(city => ({
+                    'type': 'Feature',
+                    'geometry': city.geometry,
+                    'properties': {
+                        'description': 
+                            `<strong>${city.name} `+
+                            `(${city.code})</strong><br>`+
+                            `${city.quickInfo}`
+                    }
+                })),
             }
         });
+        
         // add a layer showing the places.
         map.addLayer({
             'id': `cities-${area.name}`,
@@ -54,6 +55,30 @@ map.on('load', function () {
                 'circle-color': area2color[area.name],
                 'circle-radius': 8
             }
+        });
+        
+        // add a popup
+        let popup = new mapboxgl.Popup();
+        map.on('mouseenter', `cities-${area.name}`, function (e) {
+            let coordinates = e.features[0].geometry.coordinates.slice();
+            const description = e.features[0].properties.description;
+             
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+             
+            popup
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        });
+        
+        // remove popup
+        map.on('mouseleave', `cities-${area.name}`, function (e) {
+            popup.remove();
         });
     }    
 });
