f3e508d76ca181188bb9d3f7b5ccb196a4776178
axyorah <elvira.bozileva@gmail.com>
2021-02-02 22:37:44 +0100

    add: added simple bootstrap in-built form validation (js fun copied from bootstrap form validation doc page)

---
+++ public/js/validateForms.js
@@ -0,0 +1,20 @@
+// disable form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (evt) {
+                if (!form.checkValidity()) {
+                    evt.preventDefault()
+                    evt.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
