80a9ff7f63f87d5f5893fe8c6ca3fc778f586b36
axyorah <elvira.bozileva@gmail.com>
2021-02-15 16:36:58 +0100

    add: added `Edit Area` form at route `/areas/:id/edit`

--- controllers/area.js
+++ controllers/area.js
@@ -41,3 +41,66 @@ module.exports.addNew = async (req,res) => {
     req.flash('success', `New area "${area.name}" has been added!`);
     res.redirect('/');
 }
+
+module.exports.renderEdit = async (req,res) => {
+    const { id } = req.params;
+
+    const selected = await Area.findOne({ _id: id });
+    const cities = await City.find({});
+    const areas = await Area.find({}).populate('cities');
+
+    res.render('./areas/edit.ejs', { selected, cities, areas });
+}
+
+module.exports.updateEdited = async (req,res) => {
+    console.log('REQ.BODY.AREA:');
+    console.log(req.body.area);
+    const { id } = req.params;
+    const { name, code, color, quickInfo } = await req.body.area;
+    const generalInfo = await req.body.area['General Information'];
+    //const areaSpecific = req.body.area['Area-Specific'];
+
+    const area = await Area.findByIdAndUpdate(
+        id, 
+        {
+            name: name,
+            code: code,
+            color: color,
+            quickInfo: parseMixedSchema(quickInfo),
+            'General Information': parseMixedSchema(generalInfo),
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+
+    // mixed schema fields require explicit update
+    area.markModified('General Information'); 
+    //city.markModified('City-Specific');    
+
+    await area.save();  
+
+    req.flash('success', `"${area.name}" has been succesfully updated!`);
+    res.redirect('/');
+}
+
+module.exports.delete = async (req,res) => {
+    const { id } = req.params;
+    console.log(`DELETING ${id}`);
+
+    // delete area from Area collection
+    await Area.findByIdAndDelete(id);
+
+    // set orphaned cities to be children on default area
+    const defaultAreaId = '123'; // MAKE ACTUAL DEFAULT AREA
+    const cities = await City.updateMany(
+        { area: id },
+        { area: defaultAreaId }
+    );
+    cities.save();
+
+
+    req.flash('success', `"${area.name}" has been succesfully deleted!`);
+    res.redirect('/');
+}
--- routes/area.js
+++ routes/area.js
@@ -8,6 +8,12 @@ router.route('/areas/new')
     .get(setLocals, area.renderNew)
     .post(setLocals, area.addNew);
 
+router.route('/areas/:id/edit')
+    .get(setLocals, area.renderEdit)
+    .post(setLocals, area.updateEdited);
+
+router.route('/areas/:id/delete');
+
 router.route('/areas/:id')
     .get(setLocals, area.show);
 
