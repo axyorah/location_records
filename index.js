d4d50094f763b06226b51a72f4192d96911f95e9
axyorah <elvira.bozileva@gmail.com>
2021-02-21 18:23:22 +0100

    temp: temporary switching to local db - checkpoint before adding projects

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const { setLocals } = require('./middleware.js');
 const User = require('./models/user.js');
 
 // --- MONGOOSE SETUP --- 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/rov';
+//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/rov';
+const dbUrl = 'mongodb://localhost:27017/rov';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
