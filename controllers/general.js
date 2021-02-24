fb4a18ce29cebbd83acba3518d8d3736a6ed2583
axyorah <elvira.bozileva@gmail.com>
2021-02-24 14:35:29 +0100

    mod: home page at route `/` now contains empty map and dropdown menu for logged in user's projects

--- controllers/general.js
+++ controllers/general.js
@@ -13,15 +13,18 @@ module.exports.home = async (req,res) => {
     // by default `/` shows the list of all areas as links;
     // on area request (click area link) - shows specific area info
     // on city request (click map marker) - show specific city info
+    const { username } = res.locals;
     const { areaId } = req.query;
 
-    const areas = await Area.find({}).populate('cities');
-    const cities = await City.find({});
+    const user = await User.findOne({ username }).populate('projects');
+
+    //const areas = await Area.find({}).populate('cities');
+    //const cities = await City.find({});
 
     const selected = areaId ? await Area.findById(areaId).populate('cities') : undefined;
     
     console.log('GENERAL.HOME: SELECTED');
     console.log(selected);
 
-    res.render('./general/home.ejs', { selected, cities, areas, projects: [] });
+    res.render('./general/home.ejs', { selected, cities: [], areas: [], projects: user.projects });
 }
