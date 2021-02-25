ac0c8e4b7250069108a459a538c7f4b09a85562c
axyorah <elvira.bozileva@gmail.com>
2021-02-25 15:42:09 +0100

    fix: `setLocals()` middleware now searches for `projectId` not only in `res.cookies` but also in `res.params` to avoid triggering `isProjectDefined` middleware when project object is provided (and its id can be easily derived), but the cookies were not set yet

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ module.exports.setLocals = (req,res,next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
 
-    const { projectId } = req.cookies;
+    const projectId = req.cookies.projectId || req.params.projectId;
     res.locals.projectId = projectId;
     next();
 }
@@ -54,9 +54,11 @@ module.exports.isLoggedIn = (req,res,next) => {
 }
 
 module.exports.isProjectDefined = async (req,res,next) => {
+    console.log('ISPROJECTDEFEIND')
     if(!res.locals ||
         res.locals.projectId === undefined || 
-        res.locals.projectId === 'undefined') {
+        res.locals.projectId === 'undefined' ||
+        res.locals.projectId === '"undefined"') {
         req.flash('error', 'Select Project First');
         return res.redirect('/');
     } 
