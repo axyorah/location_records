5c8289716fc96d4b0fcb440b51c17ba4009d4e52
axyorah <elvira.bozileva@gmail.com>
2021-02-26 13:53:09 +0100

    mod(text): slightely changed the description for the default project of the newly registered user

--- utils/formUtils.js
+++ utils/formUtils.js
@@ -66,19 +66,22 @@ const createDefaultProject = ( projectToken ) => {
         description: 
             jsonEscape(
             'It seems, this is your first project. '+
-            'You can start by creating a New Area. ' +
-            'It will not be visible on the map at first, but once you Add City to it ' +
+            'Most of the controls you will need are located on the menu bar on top of the screen. ' +
+            'Additionally, there are edit and delete buttons that you can use. ' +
+            'You can start by editing this project to adjust its name, description and map settings ' +
+            '(center, zoom and style)\n\n.' +
+            'Once it is done, you can start populating the project with data. ' +
+            'Start by creating a New Area. ' +
+            'It will not be visible on the map at first, but once you add a city to it, ' +
             'a marker will appear on the map at the location that you chose for the city ' +
-            'and a color that you chose for the area.\n' +
+            'and a color that you chose for the area.\n\n' +
             'On the project home page you can view all the areas that belong to this project. ' +
-            'If you follow the area-link you will see all the cities that belong to the area.\n' +
+            'If you follow the area-link you will see all the cities that belong to the area.\n\n' +
             'You can also access the city-data from the map: ' +
-            'hover over city markers to see a short city/area note ' +
-            'or click  on a marker to seed the detailed information.\n' +
+            'hover over the city markers to see a short city/area note ' +
+            'or click  on the marker to see the detailed information.\n\n' +
             'You can always edit or delete any city or area information ' +
-            'by clicking city/area edit or delete buttons. ' +
-            'Additionally, you can change map settings (center, zoom and style) ' +
-            'by clicking the project edit button.\n' +
+            'by clicking city/area edit or delete buttons. ' +            
             'Finally, you can create a New Project and share it with other people' +
             'by sharing the Project Token, which is always visitble on the project home page. ' +
             ''
