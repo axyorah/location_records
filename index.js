7e0de4eaca0d62e27b485108b0d297e1e31f4cab
axyorah <elvira.bozileva@gmail.com>
2021-08-13 23:05:49 +0200

    mod(minor): added minor comment

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const User = require('./models/user.js');
 
 // --- MONGOOSE SETUP --- 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/rov';
-//const dbUrl = 'mongodb://localhost:27017/rov';
+// const dbUrl = 'mongodb://localhost:27017/rov'; // don't forget to start mongod!
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
